d7302f1cc561e2b3bfdd1063c878298848566e96
navyap11 <ashwin.p.parmar@gmail.com>
2024-11-21 14:55:39 +0100

    h

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+a= 5
+print ("the data type for a is: ",type(a))
+
+b= 5.333333 
+print ("the data type for b is: ",type(b))
+
+c= True
+print ("the data type for c is: ",type(c))
+
+d= "Navya"
+print ("the data type for d is: ",type(d))
+
